b1b062647df6eff65de92be3841dfa791f498d26
Anton Bossenbroek <anton.bossenbroek1@ey.com>
2015-01-28 20:17:42 -0500

    PPNRValidation/tests/testhtat/log.diffinv.R: fixed typo in test.

--- PPNRValidation/tests/testthat/log.diffinv.R
+++ PPNRValidation/tests/testthat/log.diffinv.R
@@ -2,9 +2,9 @@
 # was initially developed.
 a <- 2 : 20
 a.log.diff <- log.diff(a)
-expect_that(log.diff.inv(a.log.diff, xi = a[1]), equals(a))
+expect_that(log.diffinv(a.log.diff, xi = a[1]), equals(a))
 
 a.log.diff <- log.diff(a, base = 10)
-expect_that(log.diff.inv(a.log.diff, xi = a[1], base = 10), equals(a))
+expect_that(log.diffinv(a.log.diff, xi = a[1], base = 10), equals(a))
 
 
