d769ea13228b08c98c868fbaf6266759ad89cd06
Anton Bossenbroek <anton.bossenbroek1@ey.com>
2015-01-31 18:58:06 -0500

    Changed the package name to a more sensible name.

---
+++ ccar.development/R/ccar.development-package.R
@@ -0,0 +1,10 @@
+#' The ccar development package allows for easy development and validation of
+#' CCAR models.
+#'
+#' @name ccar.development-package
+#' @aliases ccar.development
+#' @docType package
+#' @title Simplified ccar development
+#' @author Omar Azhar, Anton Bossenbroek, David Karapetyan, Prabakar Rajasekaran
+NULL
+
