cc9a17e6f58fdd82dfffa2c34c691e1d457a0685
David Karapetyan <david.karapetyan@ey.com>
2015-02-12 18:28:44 -0500

    renamed macro munging file--removed bugs, including use of negative indicies, which not required
    Former-commit-id: de29a09b5f7a57ec9dc15211bf1f927a2918a2b9
    Former-commit-id: b0ad56c16606419c4c13df54065133e9042a9aec

--- class_model/src/macro_data_munging.r
+++ class_model/src/macro_data_munging.r
@@ -1,33 +1,50 @@
+#Take Sample of many macroeconomic variables, and munge so that we compute some
+#new variables that are crucial as inputs for our autoregression model. 
+
+
+
 setwd("C:/ppnr.quant.repo/class_model/data/")
 Base.macro <- read.csv("Base.macro.csv")
 Adverse.macro <- read.csv("Adverse.macro.csv")
 Severely.Adverse.macro <- read.csv("Severely.Adverse.macro.csv")
 
-names(Severely.Adverse.macro)
+
+
 
 Macro.Input.f <- function(scenario = "", data = "", start.Y = 2014, start.Q = 3) {
-    # data=Base.macro
-    data$Term.Spread <- data$X10.year.Treasury.yield - data$X3.Month.Treasury.Yield
+
+  #First, declare all macroeconomic variables we will generate from the input
+  #macroeconomic scenario data
+ 
+
+    data$Annualized.Real.GDP.growth <- NULL
+    data$Quarterly.change.in.10.year.Treasury.yield <- NULL
+    data$Quarterly.change.in.BBB.bond.spread <- NULL
+    data$Home.price.growth <- NULL
+    data$Commercial.Property.Price.Growth <- NULL
+    data$Stock.Market.returns <- NULL
+    data$Annualized.Change.in.Unemployment <- NULL
+
+    #Next, we run our computations for each variable, then append the variable
+    #to the end of the input market scenario spreadsheet "data"
+
+    data$Term.Spread <- (data$X10.year.Treasury.yield - data$X3.Month.Treasury.Yield)
     
-    data$Annualized.Real.GDP.growth <- NA
     for (i in (4:nrow(data))) {
         data$Annualized.Real.GDP.growth[i] <- sum(data$Real.GDP.growth[(i - 3):i])/4
     }
     
-    data$Quarterly.change.in.10.year.Treasury.yield <- NA
     data$Quarterly.change.in.10.year.Treasury.yield[2:nrow(data)] <- diff(data$X10.year.Treasury.yield)
     
-    data$Stock.Market.returns <- NA
-    data$Stock.Market.returns[-1] <- diff(log(data$Dow.Jones.Total.Stock.Market.Index..Level.))
+    data$Stock.Market.returns <- diff(log(data$Dow.Jones.Total.Stock.Market.Index..Level.))
     
-    data$Quarterly.change.in.BBB.bond.spread <- NA
-    data$Quarterly.change.in.BBB.bond.spread[-1] <- diff(data$BBB.corporate.yield)
-    
+    data$Quarterly.change.in.BBB.bond.spread <- diff(data$BBB.corporate.yield)
+   
+    #returns maximum of quarterly change, and zero
     data$Quarterly.change.in.BBB.Spread.if.change.is.positive <- ifelse(data$Quarterly.change.in.BBB.bond.spread < 
         0, 0, data$Quarterly.change.in.BBB.bond.spread)
     
-    data$Home.price.growth <- NA
-    data$Commercial.Property.Price.Growth <- NA
+    
     for (i in (5:nrow(data))) {
         data$Home.price.growth[i] <- 100 * (log(data$House.Price.Index..Level.[i]) - log(data$House.Price.Index..Level.[i - 
             4]))
@@ -42,7 +59,6 @@ Macro.Input.f <- function(scenario = "", data = "", start.Y = 2014, start.Q = 3)
     data$Commercial.Property.Price.Growth.Negative <- ifelse(data$Commercial.Property.Price.Growth < 
         0, data$Commercial.Property.Price.Growth, 0)
     
-    data$Annualized.Change.in.Unemployment <- NA
     data$Annualized.Change.in.Unemployment[-1] <- diff(data$Unemployment.rate) * 4
     
     data$Time.trend <- data$Quarter * 0.25 + (data$Year - 1991)
