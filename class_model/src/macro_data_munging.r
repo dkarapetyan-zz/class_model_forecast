3bcbd8209624a35527506bc3d8a922ef6846638e
David Karapetyan <david.karapetyan@ey.com>
2015-02-17 18:04:46 -0500

    macro munging file now compiles, albeit with some imprecise output. However, outputs all macro variables, as desired
    Former-commit-id: 54b26575997eca4bcb1aaea5e2350ef272ee09f3
    Former-commit-id: 1f666c19630da0f7d50a2fd10268d02654726034

--- class_model/src/macro_data_munging.r
+++ class_model/src/macro_data_munging.r
@@ -3,76 +3,78 @@
 
 
 
-setwd("c:/ppnr.quant.repo/class_model/data/")
-base.macro <- read.csv("base_macro.csv")
-adverse.macro <- read.csv("adverse_macro.csv")
-severely.adverse.macro <- read.csv("severely_adverse_macro.csv")
 
 
 
 
-InputMacroMunger <- function(scenario = "", data = "", start.y = 2014, start.q = 3) {
 
+
+InputMacroMunger <- function(data = "", start.y = 2014, start.q = 3) {
+  setwd("c:/ppnr.quant.repo/class_model/data/")
+  data <- read.csv(data)  
+  colnames(data) <- gsub("\\.{2}.*$", "", colnames(data))   #delete ellipses and everything after from import
+  colnames(data) <- lapply(colnames(data), tolower)      #change all variables to lowercase 
+
+  
   #first, declare all macroeconomic variables we will generate from the input
   #macroeconomic scenario data
+
+  munged.macro.data<-list()
+  munged.macro.data$annualized.real.gdp.growth <- NULL
+  munged.macro.data$quarterly.change.in.10.year.treasury.yield <- NULL
+  munged.macro.data$quarterly.change.in.bbb.bond.spread <- NULL
+  munged.macro.data$home.price.growth <- NULL
+  munged.macro.data$commercial.property.price.growth <- NULL
+  munged.macro.data$stock.market.returns <- NULL
+  munged.macro.data$annualized.change.in.unemployment <- NULL
+
+  #next, we run our computations for each variable, then append the variable
+  #to the end of the input market scenario spreadsheet "data"
+
+  munged.macro.data$term.spread <- (data$x10.year.treasury.yield - data$x3.month.treasury.yield)
+
+
+
+
+  for (i in (1:(nrow(data)-3))) {
+    munged.macro.data$annualized.real.gdp.growth[4*i] <- sum(data$real.gdp.growth[i:(i+3)])/4
+  }
+
+  munged.macro.data$quarterly.change.in.10.year.treasury.yield[2:nrow(data)] <- diff(data$x10.year.treasury.yield)
+
+  munged.macro.data$stock.market.returns <- diff(log(data$dow.jones.total.stock.market.index))
+
+  munged.macro.data$quarterly.change.in.bbb.bond.spread <- diff(data$bbb.corporate.yield)
+
+  #returns maximum of quarterly change, and zero
+  munged.macro.data$quarterly.change.in.bbb.spread.if.change.is.positive <- ifelse(data$quarterly.change.in.bbb.bond.spread < 
+                                                                      0, 0, data$quarterly.change.in.bbb.bond.spread)
+
+
+  for (i in (1:(nrow(data)-4))) { # 3 entries of NA, with fourth entry corresponding to annual growth
+    munged.macro.data$home.price.growth[4*i] <- 100 * (log(data$house.price.index[i+4]) - log(data$house.price.index[i]))
+    munged.macro.data$commercial.property.price.growth[4*i] <- 100 * (log(data$commercial.real.estate.price.index[i+4]) - 
+                                                       log(data$commercial.real.estate.price.index[i]))
+  }
+
+  munged.macro.data$home.price.growth.if.growth.is.negative <- ifelse(data$home.price.growth < 0, data$home.price.growth, 
+                                                         0)
+
+  munged.macro.data$commercial.property.price.growth.negative <- ifelse(data$commercial.property.price.growth < 
+                                                           0, data$commercial.property.price.growth, 0)
+
+  munged.macro.data$annualized.change.in.unemployment <- diff(data$unemployment.rate) * 4
+
+  munged.macro.data$time.trend <- data$quarter * 0.25 + (data$year - 1991)
+  #munged.macro.data <- munged.macro.data[which(munged.macro.data$time.trend >= (start.y - 1991) + 0.25 * start.q), ]
+  #may not be necessary,  will revisit later
  
-
-    data$annualized.real.gdp.growth <- NULL
-    data$quarterly.change.in.10.year.treasury.yield <- NULL
-    data$quarterly.change.in.bbb.bond.spread <- NULL
-    data$home.price.growth <- NULL
-    data$commercial.property.price.growth <- NULL
-    data$stock.market.returns <- NULL
-    data$annualized.change.in.unemployment <- NULL
-
-    #next, we run our computations for each variable, then append the variable
-    #to the end of the input market scenario spreadsheet "data"
-
-    data$term.spread <- (data$x10.year.treasury.yield - data$x3.month.treasury.yield)
-    
-    for (i in (4:nrow(data))) {
-        data$annualized.real.gdp.growth[i] <- sum(data$real.gdp.growth[(i - 3):i])/4
-    }
-    
-    data$quarterly.change.in.10.year.treasury.yield[2:nrow(data)] <- diff(data$x10.year.treasury.yield)
-    
-    data$stock.market.returns <- diff(log(data$dow.jones.total.stock.market.index..level.))
-    
-    data$quarterly.change.in.bbb.bond.spread <- diff(data$bbb.corporate.yield)
-   
-    #returns maximum of quarterly change, and zero
-    data$quarterly.change.in.bbb.spread.if.change.is.positive <- ifelse(data$quarterly.change.in.bbb.bond.spread < 
-        0, 0, data$quarterly.change.in.bbb.bond.spread)
-    
-    
-    for (i in (5:nrow(data))) {
-        data$home.price.growth[i] <- 100 * (log(data$house.price.index..level.[i]) - log(data$house.price.index..level.[i - 
-            4]))
-        data$commercial.property.price.growth[i] <- 100 * (log(data$commercial.real.estate.price.index..level.[i]) - 
-            log(data$commercial.real.estate.price.index..level.[i - 4]))
-        
-    }
-    
-    data$home.price.growth.if.growth.is.negative <- ifelse(data$home.price.growth < 0, data$home.price.growth, 
-        0)
-    
-    data$commercial.property.price.growth.negative <- ifelse(data$commercial.property.price.growth < 
-        0, data$commercial.property.price.growth, 0)
-    
-    data$annualized.change.in.unemployment[-1] <- diff(data$unemployment.rate) * 4
-    
-    data$time.trend <- data$quarter * 0.25 + (data$year - 1991)
-    data <- data[which(data$time.trend >= (start.y - 1991) + 0.25 * start.q), ]
-    write.csv(data, file = paste(getwd(), "/", scenario, "input_macro_munged.csv", sep = ""), 
-        row.names = false)
-    
-    print(data[1:5, ])
+  #return munged.macro.data
+  munged.macro.data
 }
 
 #Generate Output only for base macro. 
 #TODO: rewrite code later on so that it takes a user input (base, adverse, or severely adverse)
 #nd outputs the associated capital forecast
 
-InputMacroMunger(scenario = "base", data = base_macro)
-#InputMacroMunger(scenario = "adverse", data = adverse_macro)
-#InputMacroMunger(scenario = "severely.adverse", data = severely_adverse_macro) 
+InputMacroMunger("base_macro.csv")
