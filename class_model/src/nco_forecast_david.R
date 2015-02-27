ac0095f2d22e4f765f3cff1bb3b0fa3f695c9389
David Karapetyan <david.karapetyan@ey.com>
2015-02-27 16:56:51 -0500

    Draft of NCO forecasting module. Without bugs now, gives correct output with respect to BAC unit test, LLL Provision tab
    Former-commit-id: 715fa49f29687907e0f7cc55e932513fd99c4801
    Former-commit-id: 0e06ae813d1fa22e4d6ce72f93691bd89d9410a0

--- class_model/src/nco_forecast_david.R
+++ class_model/src/nco_forecast_david.R
@@ -1,9 +1,12 @@
 # Author: David Karapetyan
 ###############################################################################
-#This Function outputs a time series balance forecast (15 columns) to 2017 Q4 for Leases, Credit Cards,
-#and other variables, from a time series input file of 2014 Q3 input data for the same variables.
+#This Function outputs a time series nco forecast (15 columns) to 2017 Q4 for Leases, Credit Cards,
+#and other variables, from a series input files of 2014 Q3 input data for the same variables.
+#The function is fed, as default arguments, arima model coefficients, and macroeconomic forecasts
+#for 2014Q3.
 
-NCOForecast <- function(nco.forecast.input, model.coefficients, macro.forecasts) {
+
+NCOForecast <- function(nco.forecast.input, model.coefficients.nco, macro.forecasts) {
 	.required_colnames_forecasts <- c("FirstLien.Residential.Real.Estate", "Junior.Lien.Residential.Real.Estate", 
 			"HELOC.Residential.Real.Estate", "Construction.Commercial.Real.Estate", 
 			"Multifamily.Commercial.Real.Estate", "NonFarm.NonResidential.CRE", 
@@ -11,17 +14,14 @@ NCOForecast <- function(nco.forecast.input, model.coefficients, macro.forecasts)
 			"Loans.to.Foreign.Governments", "Agriculture", "Loans.to.Depository.Institutions", 
 			"Other")		
 	#	
-	.required_colnames_model_coefficients <-
-			c("Net.Interest.Margin", "Noninterest.Nontrading.Income.Ratio", 
-					"Return.on.Trading.Assets", "Compensation.Noninterest.Expense.Ratio", 
-					"Fixed.Asset.Noninterest.Expense.Ratio", "Other.Noninterest.Expense.Ratio", 
-					"Return.on.AFS.Securities", "FirstLien.Residential.Real.Estate", 
-					"Junior.Lien.Residential.Real.Estate", "HELOC.Residential.Real.Estate", 
-					"Construction.Commercial.Real.Estate", "Multifamily.Commercial.Real.Estate", 
-					"NonFarm.NonResidential.CRE", "Credit.Card", "Other.Consumer", 
-					"CI", "Leases", "Other.Real.Estate", "Loans.to.Foreign.Governments", 
-					"Agriculture", "Loans.to.Depository.Institutions", "Other")
-
+	.required_colnames_model.coefficients.nco <-
+			c("FirstLien.Residential.Real.Estate", "Junior.Lien.Residential.Real.Estate", 
+					"HELOC.Residential.Real.Estate", "Construction.Commercial.Real.Estate", 
+					"Multifamily.Commercial.Real.Estate", "NonFarm.NonResidential.CRE", 
+					"Credit.Card", "Other.Consumer", "CI", "Leases", "Other.Real.Estate", 
+					"Loans.to.Foreign.Governments", "Agriculture", "Loans.to.Depository.Institutions", 
+					"Other")	
+	
 	.required_colnames_macro <- c("Home.price.growth", "Commercial.Property.Price.Growth",
 			"Home.price.growth.if.growth.is.negative",
 			"Commercial.Property.Price.Growth.Negative", "Annualized.Change.in.Unemployment", 
@@ -31,8 +31,8 @@ NCOForecast <- function(nco.forecast.input, model.coefficients, macro.forecasts)
 	if (!all(.required_colnames_forecasts %in% colnames(nco.forecast.input))) {
 		stop("Not all required colnames were found in nco.forecast.input")
 	}
-	if (!all(.required_colnames_model_coefficients %in% colnames(model.coefficients))) {
-		stop("Not all required colnames were found in model.coefficients")
+	if (!all(.required_colnames_model.coefficients.nco %in% colnames(model.coefficients.nco))) {
+		stop("Not all required colnames were found in model.coefficients.nco")
 	}
 	if (!all(.required_colnames_macro %in% colnames(macro.forecasts))) {
 		stop("Not all required colnames were found in macro.forecasts")
@@ -48,23 +48,29 @@ NCOForecast <- function(nco.forecast.input, model.coefficients, macro.forecasts)
 					"Credit.Card", "Other.Consumer", "CI", "Leases", "Other.Real.Estate", 
 					"Loans.to.Foreign.Governments", "Agriculture", "Loans.to.Depository.Institutions", 
 					"Other")	
-	.nco.forecast.ts[1,] <- nco.forecast.input[1,]
 	#first column of our forecast is just our initial input data
 	.nrows<-nrow(.nco.forecast.ts) #for efficiency in looping
-	for(i in 2:.nrows) {
-		browser()
-		.nco.forecast.ts[i, ] <- model.coefficients["Intercept", ] +
-				(model.coefficients["Lagged.dependent.variable",]
-		*.nco.forecast.ts[(i-1), ]) + (model.coefficients["Home.price.growth",]*
-		macro.forecasts[i,"Home.price.growth"]) + 
-	(model.coefficients["Home.price.growth.if.growth.is.negative",]*macro.forecasts[i,"Home.price.growth"])  
+	for(i in 1:.nrows) {
+		if (i==1) { #initial data for nco assigned here
+				.nco.forecast.ts[i,] <- nco.forecast.input[i,]
+			}
+		else { # arithmetic
+			
+			.nco.forecast.ts[i, ] <- (model.coefficients.nco["Intercept", ] +
+					model.coefficients.nco["Lagged.dependent.variable",]
+						*.nco.forecast.ts[(i-1), ] +
+					model.coefficients.nco["Home.price.growth", ]* macro.forecasts[i,"Home.price.growth"] + 
+					model.coefficients.nco["Home.price.growth.if.growth.is.negative",]
+						*macro.forecasts[i,"Home.price.growth.if.growth.is.negative"])  
+		}
 	}
 	
 	return (View(.nco.forecast.ts))
 }
-load("c:/ppnr.quant.repo/class_model/data/nco_forecast_input.RData")
-load("c:/ppnr.quant.repo/class_model/data/model_coefficients.RData")
-load("c:/ppnr.quant.repo/class_model/data/macro_forecast.RData")
 
-NCOForecast(nco.forecast.input, model.coefficients, macro.forecasts)
 #for testing
+#load("c:/ppnr.quant.repo/class_model/data/nco_forecasts_input.RData")
+#load("c:/ppnr.quant.repo/class_model/data/model_coefficients_nco.RData")
+#load("c:/ppnr.quant.repo/class_model/data/macro_forecasts.RData")
+
+#NCOForecast(nco.forecast.input, model.coefficients.nco, macro.forecasts)
