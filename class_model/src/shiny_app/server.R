53c36aacf0deeabb39149a4c512fe03bb93a0a61
David Karapetyan <david.karapetyan@gmail.com>
2015-04-19 13:15:30 -0400

    Working shiny forecast plots
    Former-commit-id: e62fd0fc781a5dfc8738bb192ac5ac243cc9b815
    Former-commit-id: 7bf4e4b5212fdd6b058a5fe722f9a19467e54c0f

--- class_model/src/shiny_app/server.R
+++ class_model/src/shiny_app/server.R
@@ -3,24 +3,116 @@
 # Author: David Karapetyan
 ###############################################################################
 
+source("/Users/davidkarapetyan/Documents/code/r/ppnr.quant.repo/class_model/src/graphs/graph_forecast.R")
+library("shiny")
 
-library(shiny)
-
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-      
-      # Expression that generates a histogram. The expression is
-      # wrapped in a call to renderPlot to indicate that:
-      #
-      #  1) It is "reactive" and therefore should re-execute automatically
-      #     when inputs change
-      #  2) Its output type is a plot
-      
-      output$distPlot <- renderPlot({
-            x    <- faithful[, 2]  # Old Faithful Geyser data
-            bins <- seq(min(x), max(x), length.out = input$bins + 1)
-            
-            # draw the histogram with the specified number of bins
-            hist(x, breaks = bins, col = 'skyblue', border = 'black')
-          })
-    })
+
+			
+			
+			output$ui <- renderUI({
+						if (is.null(input$book))
+							return()
+						
+# Depending on input$book, we'll generate a different
+# UI component and send it to the client.
+						switch(input$book,
+								"Capital" = selectInput("dynamic_var", "Choose a variable:",
+										choices = c(
+												"Allowed DTA",
+												"Capital",
+												"Dividends",
+												"Leverage Ratio",
+												"Net Income",
+												"Net Income Before Tax",
+												"Taxes",
+												"Tier 1 Common Capital"),
+								selected = "Net Income"),
+								"LLL" = selectInput("dynamic_var", "Choose a variable:",
+										choices = c(
+												"Total Reserves   000 ",
+												"Provision"),
+								selected = "Provision"),
+								"AFS" =  selectInput("dynamic_var", "Choose a variable:",
+										choices = c(
+												"Return on AFS Securities",
+												"Total AFS Securities",
+												"Gain AFS Securities"),
+								selected = "Total AFS Securities"),
+								"NCO" = selectInput("dynamic_var", "Choose a variable:",
+										choices = c(
+												"FirstLien Residential Real Estate",
+												"Junior Lien Residential Real Estate",
+												"HELOC Residential Real Estate",
+												"Construction Commercial Real Estate",
+												"Multifamily Commercial Real Estate",
+												"NonFarm NonResidential CRE",
+												"Credit Card",
+												"Other Consumer",
+												"CI", 
+												"Leases",
+												"Other Real Estate",
+												"Loans to Foreign Governments",
+												"Agriculture",
+												"Loans to Depository Institutions",
+												"Other"),
+								selected = "CI"),
+								"PPNR" = selectInput("dynamic_var", "Choose a variable:",
+										choices = c(
+												"Compensation Noninterest Expense Ratio", 
+												"Fixed Asset Noninterest Expense Ratio",
+												"Net Interest Margin",
+												"Noninterest Nontrading Income Ratio",
+												"Other Noninterest Expense Ratio",
+												"Return on Trading Assets"),
+								selected = "Return on Trading Assets"),
+#								"Asset Coefficients" = selectInput("dynamic_var", "Choose a variable:",
+#										choices = c(
+#												"Net Interest Margin",
+#												"Noninterest Nontrading Income Ratio", 
+#												"Compensation Noninterest Expense Ratio",
+#												"Fixed Asset Noninterest Expense Ratio",
+#												"Other Noninterest Expense Ratio",
+#												"Return on Trading Assets"),
+#								selected = "Net Interest Margin"),
+								"Loss" =  selectInput("dynamic_var", "Choose a variable:",
+										choices = c(
+												"4-Qrt Net Charge-offs",
+												"Agriculture",
+												"CI", 
+												"Construction Commercial Real Estate",
+												"Credit Card",
+												"FirstLien Residential Real Estate",
+												"HELOC Residential Real Estate",
+												"Junior Lien Residential Real Estate",
+												"Leases",
+												"Loans to Depository Institutions",
+												"Loans to Foreign Governments",
+												"Multifamily Commercial Real Estate",
+												"NonFarm NonResidential CRE",
+												"Other",
+												"Other Consumer",
+												"Other Real Estate",
+												"Total Net Charge-offs"),
+								selected = "4-Qrt Net Charge-offs"))
+					})
+		
+					
+					
+					
+			
+			output$plot <- renderPlot(
+						GraphForecast(
+										book = input$book,
+										variable = input$dynamic_var,
+										bank = input$bank,
+										quarter = "2014Q3",
+										nco_data = nco_data,
+										ppnr_data = ppnr_data,
+										total_assets = total_assets,
+										capital_data = capital_data,
+										model_coefficients = model_coefficients_ey,
+										macro_forecasts = macro_forecasts)
+					)				
+		})
+
--- class_model/src/shiny_app/ui.R
+++ class_model/src/shiny_app/ui.R
@@ -1,29 +1,67 @@
-# TODO: Createe RShiny version of graph_forecast ui, and deploy
+# todo: createe rshiny version of graph_forecast ui, and deploy
 # 
-# Author: David Karapetyan
+# author: david karapetyan
 ###############################################################################
+load("/Users/davidkarapetyan/Documents/code/r/ppnr.quant.repo/class_model/data/nco_data.RData")
 
-library(shiny)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-        
-        # Application title
-        titlePanel("Hist"),
-        
-        # Sidebar with a slider input for the number of bins
-        sidebarLayout(
-            sidebarPanel(
-                sliderInput("bins",
-                    "Number of bins:",
-                    min = 1,
-                    max = 50,
-                    value = 30)
-            ),
-            
-            # Show a plot of the generated distribution
-            mainPanel(
-                plotOutput("distPlot")
-            )
-        )
-    ))
+
+# define ui for application that draws a histogram
+shinyUI(
+		fluidPage(
+				
+				# application title
+				titlePanel("CLASS Model Forecasting"),
+				
+				fluidRow(
+						column(3, wellPanel(selectInput(
+												inputId = "bank",
+												label = "Choose a bank to display",
+												choices = c(
+														"Ally Financial Inc.",
+														"American Express Company",
+														"Bank of America Corporation", 
+														"Barclays Delaware Holdings LLC",
+														"BB&T Corporation",
+														"Capital One Financial Corporation",
+														"Citigroup Inc.",
+														"Discover Financial Services",
+														"General Electric Capital Corporation", 
+														"JPMorgan Chase & Co.",
+														"Santander Holdings USA, Inc.",
+														"TD Bank US Holding Company", 
+														"U.S. Bancorp",
+														"United Services Automobile Association",
+														"Wells Fargo & Company", 
+														"PNC Financial Services Group, Inc.",
+														"HSBC North America Holdings Inc.", 
+														"Citizens Financial Group, Inc.",
+														"Fifth Third Bancorp",
+														"First National of Nebraska, Inc.", 
+														"KeyCorp",
+														"SunTrust Banks, Inc.",
+														"Popular, Inc.",
+														"United National Corporation", 
+														"Bank of New York Mellon Corporation"),
+												selected = "Bank of America Corporation"
+										))),
+						column(3, wellPanel(selectInput(
+												inputId = "book",
+												label = "Choose a book to display:",
+												choices = c(
+														"Capital",
+														"PPNR",
+														"AFS",
+														"LLL",
+														"Loss",
+														"NCO"
+#														"Asset Coefficients"
+																),
+												selected = "Capital"
+										))),
+						column(3, wellPanel(uiOutput("ui"))) 
+				),
+				mainPanel(
+						plotOutput("plot")
+				)
+		))	
